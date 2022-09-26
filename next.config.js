20920d2aeb3219948d73914a3fe9f25dd912a6a6
crisnil <cacuyado@gmail.com>
2022-09-26 15:47:22 +0800

    base path

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images:{unoptimized:true}
+  images:{unoptimized:true},
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
 
 module.exports = nextConfig
+
